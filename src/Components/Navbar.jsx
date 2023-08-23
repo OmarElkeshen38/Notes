41eb89d3a9415e5289eb25f3eedb62a90fde06ed
OmarElkeshen38 <oelkeshen5@gmail.com>
2023-08-23 04:50:39 +0300

    7 commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar(props) {
     <>
         <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
             <div className="container">
-                <a className="navbar-brand" href="/">Notes</a>
+                <Link className="navbar-brand" to="home">Notes</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
